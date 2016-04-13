3dc9bb8d0da4f672ed02fa098ca09d4d0bf23977
兰追 <dongsheng.yds@alibaba-inc.com>
2016-04-13 21:03:25 +0800

    learn koa.js

--- koa-demo/app.js
+++ koa-demo/app.js
@@ -8,31 +8,48 @@ const app = koa();
 // set context for global use in lifecycle
 app.context.name = 'YDSS';
 
-app.use(function *(next) {
-    let start = new Date;  
+// app.use(function *(next) {
+//     let start = new Date;  
+//     yield next;
+//     let ms = new Date - start;
+// 
+//     console.log('x-response-time');
+//     this.set('X-Response-Time', ms + 'ms');
+// });
+// 
+// app.use(function *(next) {
+//     yield next;
+// 
+//     console.log('test order');
+// });
+// 
+// app.use(function *() {
+//     this.cookies.set('name', 'tobi');
+//     this.body = 'Hello World';
+// });
+app.use(function *(next){
+    console.log('>> one');
     yield next;
-    let ms = new Date - start;
-
-    console.log('x-response-time');
-    this.set('X-Response-Time', ms + 'ms');
-});
-
-app.use(function *(next) {
-    yield next;
-
-    console.log('test order');
+    console.log('<< one');
 });
 
 app.use(function *() {
-    this.cookies.set('name', 'tobi');
-    this.body = 'Hello World';
+    console.log('>> two');
+    this.body = 'two';
+    // yield next;
+    console.log('<< two');
 });
 
+app.use(function *(next){
+    console.log('>> three');
+    yield next;
+    console.log('<< three');
+});
 // error handler
 // if (app.env.development) {
-    app.on('error', (err, cxt) => {
-        console.log(err);
-    });
+app.on('error', (err, cxt) => {
+    console.log(err);
+});
 //}
 
 app.listen(3000);
