413bea362c700c210195808eb592647d5343d407
YDSS <y.dsheng10@gmail.com>
2016-05-27 07:37:18 +0800

    write transfer component

--- vue-demo/conf.js
+++ vue-demo/conf.js
@@ -1,5 +1,6 @@
 module.exports = {
     DIST: './dist',
-    ENTRY: './src/app/app.js',
+    // ENTRY: './src/app/app.js',
+    ENTRY: './src/transfer/index.js',
     HTML: './src/index.html'
 };
---
+++ vue-demo/src/transfer/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+
+import './index.css';
+
+let vm = new Vue({
+    el: '#main',
+    data: {
+        leftItems: ['a', 'b', 'c'],
+        rightItems: ['d', 'e', 'f'],
+        leftChecks: [],
+        rightChecks: []
+    },
+    methods: {
+        toRight: function (ev) {
+            this.rightItems = this.rightItems.concat(this.leftChecks);
+            this.rightItems.filter(e => {
+                let flag = true;
+
+                this.leftChecks.map(item => {
+                    item === e && flag = false;
+                });
+
+                return flag;
+            });
+        },
+        toLeft: function (ev) {
+            this.leftItems = this.leftItems.concat(this.rightChecks);
+            this.leftItems.filter(e => {
+                let flag = true;
+
+                this.rightChecks.map(item => {
+                    item === e && flag = false;
+                });
+
+                return flag;
+            });
+        }
+    }
+});
+
+
