186e863457d804c66178d6a02b19e8f3c048d1b1
YDSS <y.dsheng10@gmail.com>
2016-06-07 07:27:18 +0800

    commit an answer of exercise

--- data-structure-demo/pow.c
+++ data-structure-demo/pow.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 long int f_pow(long int m, unsigned int n);
+unsigned int count = 0;
 
 int main()
 {
     long int m = 2;
-    unsigned int n = 16;
+    unsigned int n = 62;
 
     printf("%ld", f_pow(m, n));
 }
 
 long int f_pow(long int m, unsigned int n)
 {
+    count ++;
+    printf("count: %d\n", count);
+    printf("n: %d\n", n);
     if (n == 0) return 1;
     if (n % 2 == 0) return f_pow(m * m, n / 2);
     else return f_pow(m * m, n / 2) * m;
--- data-structure-demo/test.c
+++ data-structure-demo/test.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-    printf("%d", 1 / 2);
+void Swap(long la, long lb);
+unsigned int RandInt(int range);
+
+int main() 
+{
+    unsigned int N = 10;
+    unsigned int A[N];
+
+    srand(time(0));
+    for (int i = 0; i < N; i++) 
+        A[i] = i + 1;
+
+    for (int i = 1; i < N; i++)
+        Swap(&A[i], &A[RandInt(0, i)]);
+}
+
+void Swap(long la, long lb)
+{
+    unsigned int tmp;
+    tmp = *la;
+    *la = *lb;
+    *lb = tmp;
+}
+
+unsigned int RandInt(int range) 
+{
+    return rand() % range;
 }
